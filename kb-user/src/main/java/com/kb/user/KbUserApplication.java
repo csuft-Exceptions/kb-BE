af5608560d6b9986e7876b18bbe3248caf56f562
yuuki <1276260753@qq.com>
2022-06-16 17:08:45 +0800

    bug fixes & userInfo init

---
+++ kb-user/src/main/java/com/kb/user/KbUserApplication.java
@@ -0,0 +1,18 @@
+package com.kb.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mawz
+ * @version 1.0
+ * @date
+ */
+@SpringBootApplication
+public class KbUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbUserApplication.class, args);
+	}
+
+}
---
+++ kb-user/src/test/java/com/kb/user/KbUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
