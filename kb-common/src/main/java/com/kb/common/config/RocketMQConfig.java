3075b5f36f70b77479543477aefc2d12f95dd21a
yuuki <1276260753@qq.com>
2022-07-22 21:50:03 +0800

    用户动态初始化

--- kb-common/src/main/java/com/kb/common/config/RocketMQConfig.java
+++ kb-common/src/main/java/com/kb/common/config/RocketMQConfig.java
@@ -3,8 +3,11 @@ package com.kb.common.config;
 import com.kb.common.exception.InfoException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,9 +49,17 @@ public class RocketMQConfig {
         // *代表订阅所有
         try {
             consumer.subscribe("Topic-moments","*");
+            // 监听者,注册并发的监听器
+            consumer.registerMessageListener((MessageListenerConcurrently) (list, consumeConcurrentlyContext) -> {
+                for(MessageExt msg:list){
+                    log.info(msg.toString());
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            });
+            consumer.start();
         } catch (MQClientException e) {
-            log.error("接收moments消息失败",e);
-            throw new InfoException("接收moments消息失败",e);
+            log.error("接收moments消息失败,消费者出现异常",e);
+            throw new InfoException("接收moments消息失败,消费者出现异常",e);
         }
         return consumer;
     }
---
+++ kb-user/src/main/java/com/kb/user/controller/UserMomentController.java
@@ -0,0 +1,9 @@
+package com.kb.user.controller;
+
+/**
+ * @author mawz
+ * @version 1.0
+ * @date 2022-07-22 - 21:47
+ */
+public class UserMomentController {
+}
