d9f1f1ec20c9c5798b913997d8dc208cd94809a4
杨奎 <yangkui@xsyxsc.com>
2022-08-04 15:59:14 +0800

    add: es video

--- kb-video/src/main/java/com/kb/video/feign/SearchFeign.java
+++ kb-video/src/main/java/com/kb/video/feign/SearchFeign.java
@@ -3,13 +3,12 @@ package com.kb.video.feign;
 
 import com.kb.common.base.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @FeignClient(value = "search")
 public interface SearchFeign {
 
-    @GetMapping("/es-videos")
-    BaseResponse getEsVideos(@RequestParam String keyword);
+    @PostMapping("/es-addVideo")
+    public BaseResponse addVideo(Video video);
 }
